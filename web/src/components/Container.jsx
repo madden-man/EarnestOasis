24e11b5927ecbf120c31a16b71874e4d270e9daf
Tommy Madden <madde120@mail.chapman.edu>
2021-10-02 16:15:32 -0600

    Handled your silly problems

--- web/src/components/Container.jsx
+++ web/src/components/Container.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { fetchAssetList } from '../utils';
 
 export const Container = ({ children }) => {
-  const files = fetchAssetList();
-
   const background = {
     background: 'linear-gradient(to bottom, #3a7bd5, #00d2ff)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   };
